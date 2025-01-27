ffed38964b97d11630cfa1615cf3256ab6cd38f9
Ludovic Delafontaine <ludovic.delafontaine@gmail.com>
2025-01-27 13:39:25 +0000

    Fix script for quiz files

--- build-all-presentations.sh
+++ build-all-presentations.sh
@@ -6,8 +6,9 @@ MARP_DOCKER_IMAGE="marpteam/marp-cli:v4.1.1"
 
 ## Script
 
-echo "Removing all previous generated presentations..."
+echo "Removing all previous generated presentations and quizzes..."
 rm -f **/*/*-presentation.pdf || true
+rm -f **/*/*-quiz.pdf || true
 rm -f **/*/index.html || true
 
 # Check if Marp is installed locally
@@ -21,10 +22,10 @@ fi
 
 # Convert presentations
 echo "Converting presentations to HTML..."
-eval "$MARP_CMD --parallel $(nproc) **/*-presentation/README.md"
+eval "$MARP_CMD --parallel $(nproc) **/*-presentation/README.md **/*-quiz/README.md"
 
 echo "Converting presentations to PDF..."
-eval "$MARP_CMD --parallel $(nproc) --pdf **/*-presentation/README.md"
+eval "$MARP_CMD --parallel $(nproc) --pdf **/*-presentation/README.md **/*-quiz/README.md"
 
 # Rename files
 echo "Renaming HTML files to 'index.html'..."
@@ -32,11 +33,21 @@ find . -mindepth 3 -maxdepth 3 -type f -name "README.html" -exec sh -c '
     mv "$1" "$(dirname "$1")/index.html"
 ' sh {} \;
 
-echo "Renaming PDF files to match parent directory..."
-find . -mindepth 3 -maxdepth 3 -type f -name "README.pdf" -exec sh -c '
-    file="$1"
-    chapter_name=$(basename "$(dirname "$(dirname "$file")")")
-    mv "$file" "$(dirname "$1")/$chapter_name-presentation.pdf"
-' sh {} \;
+echo "Renaming presentation files to match parent directory..."
+find . -mindepth 3 -maxdepth 3 -path "*/01-presentation/README.pdf" -exec sh -c '
+    for file; do
+        chapter_name=$(basename "$(dirname "$(dirname "$file")")")
+        mv "$file" "$(dirname "$file")/$chapter_name-presentation.pdf"
+    done
+' sh {} +
 
-echo "All presentations processed successfully!"
+echo "Renaming quiz files to match parent directory..."
+find . -mindepth 3 -maxdepth 3 -path "*/03-quiz/README.pdf" -exec sh -c '
+    for file; do
+        chapter_name=$(basename "$(dirname "$(dirname "$file")")")
+        mv "$file" "$(dirname "$file")/$chapter_name-quiz.pdf"
+    done
+' sh {} +
+
+echo "All presentations and quizzes processed successfully!"
+
